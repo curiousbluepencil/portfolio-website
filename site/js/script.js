76d8c0df5e970e3d54b742a0b9cb277764442478
curiousbluepencil <nura@nuras-MacBook-Pro.local>
2022-05-06 16:09:50 +0500

    changes

--- site/js/script.js
+++ site/js/script.js
@@ -1,9 +1,42 @@
+var content = document.getElementById('content');
+var heading = document.createElement('h1');
+heading.innerText = "This is the Gallery Page";
+heading.setAttribute('style',"font-family:alice_in_wonderlandregular");
+content.appendChild(heading)
+var gallery = document.getElementById('gallery');
 
-    const hamburgerMenu = document.querySelector('.hamburger'); //Selects the element with the class name of hamburger.
-    //Creating an arrow function for changing the class to active.
-    const menuIsActive = () => {
-      //Selecting the hamburger menu and adding and removing the class of active on click.
-      hamburgerMenu.classList.toggle('active');
-    };
-    //Adds an event listener of 'click' to the hamburger menu. Then we pass in the function we created where the class is toggled on and off.
-    hamburgerMenu.addEventListener('click', menuIsActive);
+/*fetch('JSON/images.json').then(function(response) {
+    response.json().then(function(json) {
+
+        for(var i = 0; i < json.Images.length; i++){
+            console.log(i);
+            var divCol = document.createElement('div');
+            divCol.class = "col"
+
+            var image = document.createElement('img');
+            var divCard = document.createElement('div');
+            divCard.class = "card";
+            divCard.style = "width:30%";
+
+            var aImg = document.createElement('a');
+            aImg.href = json.Images[i].URL;
+
+            aImg.appendChild(image);
+
+            var divCardBody = document.createElement('div');divCard.class = "card-body";
+            var caption = document.createElement('h4');
+            caption.innerText = json.Images[i].Caption;
+            caption.style = "text-align:center";
+            image.src = json.Images[i].URL;
+            image.alt = json.Images[i].Caption;
+            image.style = "width:100%";
+            image.class = 'card-img-top';
+            divCardBody.appendChild(caption);
+            divCard.appendChild(aImg);
+            divCard.appendChild(divCardBody);
+            divCol.appendChild(divCard);
+            gallery.appendChild(divCol);
+        }
+
+    });
+});*/
